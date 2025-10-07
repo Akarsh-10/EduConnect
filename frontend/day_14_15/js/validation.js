0e2363d618fb912bc89f7337cf35f64aa359cba7
username <wecp-dummy@gmail.com>
2025-10-07 13:09:35 +0000

    Day 15 Task

--- frontend/day_14_15/js/validation.js
+++ frontend/day_14_15/js/validation.js
@@ -2,6 +2,11 @@ function login() {
    
     // You can perform login validation and authentication here
     // For simplicity, let's just display an alert
+    let testUser = document.getElementById("loginUsername").value;
+    let testPassword = document.getElementById("loginPassword").value;
+    console.log(`Login clicked. Username: ${testUser}, Password: ${testPassword}`);
+    
+
     
 }
 
@@ -9,6 +14,13 @@ function register() {
    
 
     // Frontend validation for registration form
+    const name=document.getElementById("registerName").value;
+    const username=document.getElementById("registerUsername").value;
+    const password=document.getElementById("registerPassword").value;
+    const email=document.getElementById("registerEmail").value;
+    console.log(`Register clicked. Name: ${name}, Email: ${email}, Username: ${username}, Password: ${password}`)
+
+    
     
 
     // Validate email format
